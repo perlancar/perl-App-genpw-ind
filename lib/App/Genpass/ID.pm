54a7e59bc23484707b8f276bde12127a618514a3
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-02 11:34:10 +0700

    Use App::Genpass::WordList as base (add option 'patterns')

--- lib/App/Genpass/ID.pm
+++ lib/App/Genpass/ID.pm
@@ -7,45 +7,34 @@ use 5.010001;
 use strict;
 use warnings;
 
+use App::Genpass::WordList ();
+
 our %SPEC;
 
 $SPEC{genpass} = {
     v => 1.1,
     summary => 'Generate password from combination of Indonesian words',
+    description => $App::Genpass::WordList::SPEC{genpass}{description},
     args => {
         num => {
             schema => ['int*', min=>1],
             default => 1,
             cmdline_aliases => {n=>{}},
         },
+        %App::Genpass::WordList::arg_patterns,
     },
     examples => [
     ],
 };
 sub genpass {
-    require App::wordlist;
 
     my %args = @_;
 
-    my $num = $args{num} // 1;
-
-    my $res = App::wordlist::wordlist(
+    App::Genpass::WordList::genpass(
+        (num => $args{num})           x !!defined($args{num}),
+        (patterns => $args{patterns}) x !!defined($args{patterns}),
         wordlists => ['ID::KBBI'],
-        random    => 1,
-        num       => 2*$num,
-        min_len   => 6,
     );
-    return $res unless $res->[0] == 200;
-
-    my @pass;
-    for my $i (1..$num) {
-        my $w1 = shift @{$res->[2]};
-        my $w2 = shift @{$res->[2]};
-        my $num1 = 1000 + int(9000*rand());
-        push @pass, $w1 . $num1 . $w2;
-    }
-
-    [200, "OK", \@pass];
 }
 
 1;
